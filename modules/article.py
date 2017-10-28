aa6f389e2b75bb6675881e934361459f5c1832d0
fterh <fabianterh@gmail.com>
2017-10-27 18:22:12 -0700

    Requests now include a user-agent header

--- modules/article.py
+++ modules/article.py
@@ -4,7 +4,11 @@ from bs4 import BeautifulSoup as bs
 class article:
 
     def __init__(self, url, site):
-        r = requests.get(url)
+        header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 \
+        Safari/537.36"}
+
+        r = requests.get(url, header=header)
 
         if (r.status_code != 200): # if GET request fails
             print("Error: " + str(r.status_code))
